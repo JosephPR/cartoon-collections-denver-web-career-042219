dcc1d61f88123649bf9a36f49e76894f27ec64e9
Joseph Reilly <reilly4181@gmail.com>
2019-04-09 22:07:09 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
 def roll_call_dwarves(arr)
-arr.each_with_index{|a,index| puts  "#{index} #{a}"}
+arr.each_with_index{|a,index| puts  "#{index +1 } #{a}"}
 end
 
 def summon_captain_planet# code an argument here
