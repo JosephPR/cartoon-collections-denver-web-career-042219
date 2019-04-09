e4513abea63b12a5c125459c82d82d4d5c086c05
Joseph Reilly <reilly4181@gmail.com>
2019-04-09 22:33:37 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ arr.each_with_index{|a,index| puts  "#{index +1 } #{a}"}
 end
 
 def summon_captain_planet(arr)
-  arr.collect{|a| puts a.capitalize"!"}
+  arr.collect{|a| puts a.capitalize}
 end
 
 def long_planeteer_calls())
